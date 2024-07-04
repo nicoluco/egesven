8e1e0632c84654235de4a97f94f619b63c74ce26
fepardo <142046550+fepardo@users.noreply.github.com>
2024-07-04 18:47:52 -0400

    creados algunos html y sus views y cositas

--- egesven/urls.py
+++ egesven/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
-from .views import productos
+from .views import productos,login,registro,perfil,ListaUsuario,ListaProducto
 
-urlpatterns = [path('productos', productos, name="productos"), 
+urlpatterns = [
+    path('productos', productos, name="productos"),
+    path('login', login, name="login"),
+    path('registro', registro, name="registro"),
+    path('perfil', perfil, name="perfil"),
+    path('ListaUsuario', ListaUsuario, name="ListaUsuario"),
+    path('ListaProducto', ListaProducto, name="ListaProducto"),
+
 ]
--- egesven/views.py
+++ egesven/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def productos(request):
     return render(request, 'index.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def registro(request):
+    return render(request, 'registro.html')
+
+def perfil(request):
+    return render(request, 'perfil.html')
+
+def ListaProducto(request):
+    return render(request, 'ListaProducto.html')
+
+def ListaUsuario(request):
+    return render(request, 'ListaUsuario.html')
